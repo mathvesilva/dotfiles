831783e4e8920b7d6aa15567033419b684780d4c
Matheus Silva <mathvesilva@gmail.com>
2017-03-09 23:57:03 -0300

    Add initial version of bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function syncDotFiles() {
+	  rsync --exclude ".git/" \
+		      --exclude "bootstrap.sh" \
+		      --exclude "README.md" \
+		      --exclude "LICENSE" \
+		      --exclude ".directory" \
+		      -avh --no-perms . ~;
+	  source ~/.bash_profile;
+}
+
+syncDotFiles;
+
+unset syncDotFiles;
